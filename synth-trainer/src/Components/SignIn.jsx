1828382042cffb66d7069674c13582e593d59cfb
whynot1597PC <ja_goldberg@yahoo.com>
2020-10-07 21:16:33 -0700

    Center and format card

--- synth-trainer/src/Components/SignIn.jsx
+++ synth-trainer/src/Components/SignIn.jsx
@@ -1,12 +1,26 @@
 import React, { useState } from "react";
 import { Link } from "@reach/router";
 import { auth } from "../firebase";
-import { Button, Card, CardColumns, Form, FormControl, InputGroup } from "react-bootstrap";
+import {
+  Button,
+  Card,
+  Col,
+  Form,
+  FormControl,
+  FormGroup,
+  InputGroup,
+  Row,
+} from "react-bootstrap";
+import "./SignIn.css";
+import { useEffect } from "react";
 
 const SignIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
+  const [isValidEmail, setIsValidEmail] = useState(true);
+  const emailRegExp = new RegExp('[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}', 'i');
+
   const signInWithEmailAndPasswordHandler = (event, email, password) => {
     event.preventDefault();
     auth.signInWithEmailAndPassword(email, password).catch((error) => {
@@ -19,6 +33,7 @@ const SignIn = () => {
     const { name, value } = event.currentTarget;
 
     if (name === "userEmail") {
+      emailRegExp.test(value) ? setEmail(value) && setIsValidEmail(true) : setIsValidEmail(false);
       setEmail(value);
     } else if (name === "userPassword") {
       setPassword(value);
@@ -27,59 +42,70 @@ const SignIn = () => {
 
   return (
     <div>
-      <h1>Sign In</h1>
       <div>
         {error !== null && <div>{error}</div>}
-        <div class="row h-100">
+        <div id="whole-page" class="row">
           <div class="col-sm-12 my-auto">
-        <Card className="text-center">
-        <Form >
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <FormControl
-              type="email"
-              name="userEmail"
-              value={email}
-              placeholder="E.g: faruq123@gmail.com"
-              id="userEmail"
-              onChange={(event) => onChangeHandler(event)}
-            />
-          </Form.Group>
+            <Card id="sign-in-card" className="text-center w-50">
+              <Card.Title id="sign-in-label">Sign In</Card.Title>
+              <Form>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <FormControl
+                    type="email"
+                    name="userEmail"
+                    value={email}
+                    placeholder="E.g: faruq123@gmail.com"
+                    id="userEmail"
+                    isInvalid={!isValidEmail}
+                    onChange={(event) => onChangeHandler(event)}
+                  />
+                </Form.Group>
 
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <FormControl
-              type="password"
-              name="userPassword"
-              value={password}
-              placeholder="Your Password"
-              id="userPassword"
-              onChange={(event) => onChangeHandler(event)}
-            />
-          </Form.Group>
-          <Form.Group>
-            <Button
-              onClick={(event) => {
-                signInWithEmailAndPasswordHandler(event, email, password);
-              }}
-            >
-              Sign in
-            </Button>
-          </Form.Group>
+                <Form.Group controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Row>
+                    <Col xs={8}>
+                      <FormControl
+                        type="password"
+                        name="userPassword"
+                        value={password}
+                        placeholder="Your Password"
+                        id="userPassword"
+                        onChange={(event) => onChangeHandler(event)}
+                      />
+                    </Col>
+                    <Col>
+                      <Button href="passwordReset">Forgot Password?</Button>
+                    </Col>
+                  </Row>
+                </Form.Group>
+                <Form.Group>
+                  <Button
+                    onClick={(event) => {
+                      signInWithEmailAndPasswordHandler(event, email, password);
+                    }}
+                  >
+                    Sign in
+                  </Button>
+                </Form.Group>
 
-          <Form.Group>
-            <Button variant="primary">Sign in with Google</Button>
-          </Form.Group>
-        </Form>
-        </Card>
+                <Form.Group>
+                  <Button variant="primary">Sign in with Google</Button>
+                </Form.Group>
+              </Form>
+              <Form.Group>
+                Don't have an account?{" "}
+                <Button id="signup-button" href="signUp">
+                  Sign up here
+                </Button>
+              </Form.Group>
+              <FormGroup>
+                <Button href="passwordReset">Forgot Password?</Button>
+              </FormGroup>
+            </Card>
+          </div>
         </div>
-        </div>
-        <InputGroup>
-          <p>
-            Don't have an account? <Link to="signUp">Sign up here</Link> <br />{" "}
-            <Link to="passwordReset">Forgot Password?</Link>
-          </p>
-        </InputGroup>
       </div>
     </div>
   );
