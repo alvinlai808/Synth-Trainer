ba0d4556bbde2d76f7ef136aa9fb6ba5bfbbaf73
Andrey_Varakin <andreickin@gmail.com>
2020-10-23 13:38:42 -0700

    Imported different knob library. Added Envelope Panel

--- synth-trainer/src/Components/Sandbox.jsx
+++ synth-trainer/src/Components/Sandbox.jsx
@@ -7,16 +7,25 @@ import {
   FormControl,
   InputGroup,
 } from "react-bootstrap";
-import Knob from "react-simple-knob";
+import { Knob } from "react-rotary-knob";
 
 
 const Sandbox = () => {
   const [waveform, setWaveform] = useState("sawtooth")
-  const [attackValue, setAttackValue] = useState(0.01);
-  const [decayValue, setDecayValue] = useState(1.0);
-  const [sustainValue, setSustainValue] = useState(1.0);
-  const [releaseValue, setReleaseValue] = useState(1.0);
+  let attackValue = 0.01;
+  let decayValue = 1.0;
+  let sustainValue = 1.0;
+  let releaseValue = 1.0;
 
+  //This is how we'd do it with hooks, but it seems like using 
+  //hooks inside of class components is not great practice and
+  //requires a work-around using HOC wrappers. I spent a LOT
+  //of time trying to get that to work with no luck...
+  //give it a shot if you want, I can try helping if needed -Andrey
+  // const [attackValue, setAttackValue] = useState(0.01);
+  // const [decayValue, setDecayValue] = useState(1.0);
+  // const [sustainValue, setSustainValue] = useState(1.0);
+  // const [releaseValue, setReleaseValue] = useState(1.0);
 
   const playTone = () => {
     const synth = new Tone.MonoSynth({
@@ -43,17 +52,61 @@ const Sandbox = () => {
     }
   }
 
-  const style = {
-    margin: "0.05%",
-    height: "65px",
-    fontFamily: "Arial",
-    color: "black" // Sets font color of value and knob name
-  };
+  function knobHandler() {
+    const { name, value } = this
+    if (name === "Attack") {
+      attackValue = value/1000
+    }
+    if (name === "Decay") {
+      decayValue = value/1000
+    }
+    if (name === "Sustain") {
+      sustainValue = value/1000
+    }
+    if (name === "Release") {
+      releaseValue = value/1000
+    }
+  }
+
+  class LimitedKnob extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        value: 0
+      };
+      this.handleOnChange = this.handleOnChange.bind(this);
+    }
+  
+    handleOnChange(val) {
+      //ignore change if distance is greater than defined
+      //here we use a distance of 200 because our max value is 1000
+      //change if needed
+      const maxDistance = 200;
+      let distance = Math.abs(val - this.state.value);
+      if (distance > maxDistance) {
+        return;
+      } else {
+        this.setState({ value: val });
+      }
+      
+    }
+    render() {
+      let { value, ...rest } = this.props;
+      return (
+        <div>
+          {this.state.value.toFixed(0)} ms
+          <div>
+            <Knob value={this.state.value} onChange={this.handleOnChange} {...rest} />
+          </div>
+        </div>
+      );
+    }
+  }
 
   return (
     <div className="col-sm-12 my-auto">
       <h1 className="text-3xl mb-2 text-center font-bold">Sandbox</h1>
-      <Card id="sandbox-card" className="text-center w-center">
+      <Card id="sandbox-card" className="text-center w-center" bg="light">
         <Card.Title id="sandbox-label">*Insert Synth Type*</Card.Title>
         <Form>
           <Button onClick={(event) => {playTone();}}>Hello, Tone!</Button>
@@ -61,66 +114,75 @@ const Sandbox = () => {
           <Button name="sawtoothButton" onClick={(event) => buttonClickHandler(event)}>Saw</Button>
         </Form>
       </Card>
-      <Card id="envelope-card" className="text-center w-center">
-        <Card.Title id="envelope-lable">Amplitude Envelope</Card.Title>
+      <div class="mx-auto" style={{width: "290px"}}>
+      <Card id="envelope-card" className="text-center w-center" bg="info" style={{ width: '18rem' }}>
+        <Card.Title id="envelope-label">ADSR Envelope</Card.Title>
         <Form>
-          <Knob
-            name="Volume"
-            unit="dB"
-            defaultPercentage={0.7}
-            onChange={console.log}
-            bg="gray"
-            fg="white"
-            mouseSpeed={5}
-            transform={p => parseInt(p * 50, 10) - 50}
-            style={style} 
-          />
-          <Knob
-            name="Attack"
-            unit="ms"
-            defaultPercentage={0.7}
-            onChange={console.log}
-            bg="gray"
-            fg="white"
-            mouseSpeed={5}
-            transform={p => parseInt(p * 50, 10) - 50}
-            style={style} 
-          />
-          <Knob
-            name="Decay"
-            unit="ms"
-            defaultPercentage={0.7}
-            onChange={console.log}
-            bg="gray"
-            fg="white"
-            mouseSpeed={5}
-            transform={p => parseInt(p * 50, 10) - 50}
-            style={style} 
-          />
-          <Knob
-            name="Sustain"
-            unit="ms"
-            defaultPercentage={0.7}
-            onChange={console.log}
-            bg="gray"
-            fg="white"
-            mouseSpeed={5}
-            transform={p => parseInt(p * 50, 10) - 50}
-            style={style} 
-          />
-          <Knob
-            name="Release"
-            unit="ms"
-            defaultPercentage={0.7}
-            onChange={console.log}
-            bg="gray"
-            fg="white"
-            mouseSpeed={5}
-            transform={p => parseInt(p * 50, 10) - 50}
-            style={style} 
-          />
+          <div class="row">
+            <div class="col">
+            <h3 className="text-3xl mb-2 text-center font-bold">Attack</h3>
+            <LimitedKnob
+              name="Attack"
+              value={100}
+              style={{ display: "inline-block" }}
+              min={0}
+              max={1000}
+              unlockDistance={0}
+              preciseMode={false}
+              onEnd={knobHandler}
+              width={200}
+              height={200}
+            />
+            </div>
+            <div class="col">
+            <h3 className="text-3xl mb-2 text-center font-bold">Decay</h3>
+            <LimitedKnob
+              name="Decay"
+              value={100}
+              style={{ display: "inline-block" }}
+              min={0}
+              max={1000}
+              unlockDistance={0}
+              preciseMode={false}
+              onEnd={knobHandler}
+              width={200}
+              height={200}
+            />
+            </div>
+            <div class="col">
+            <h3 className="text-3xl mb-2 text-center font-bold">Sustain</h3>
+            <LimitedKnob
+              name="Sustain"
+              value={100}
+              style={{ display: "inline-block" }}
+              min={0}
+              max={1000}
+              unlockDistance={0}
+              preciseMode={false}
+              onEnd={knobHandler}
+              width={200}
+              height={200}
+            />
+            </div>
+            <div class="col">
+            <h3 className="text-3xl mb-2 text-center font-bold">Release</h3>
+            <LimitedKnob
+              name="Release"
+              value={100}
+              style={{ display: "inline-block" }}
+              min={0}
+              max={1000}
+              unlockDistance={0}
+              preciseMode={false}
+              onEnd={knobHandler}
+              width={200}
+              height={200}
+            />
+            </div>
+          </div>
         </Form>
       </Card>
+      </div>
     </div>
   );
 }
