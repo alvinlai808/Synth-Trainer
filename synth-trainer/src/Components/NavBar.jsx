3c48337d310dd46c497be7d932a35df0a8e9df9d
David Sackler <davidsackler@Davids-MacBook-Pro.local>
2020-10-28 15:22:12 -0700

    Changed dropdown to modules

--- synth-trainer/src/Components/NavBar.jsx
+++ synth-trainer/src/Components/NavBar.jsx
@@ -22,12 +22,10 @@ const NavigationBar = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown title="Modules" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Module 1</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Module 2</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Module 3</NavDropdown.Item>
             <NavDropdown.Divider />
             <NavDropdown.Item href="#action/3.4">
               Separated link{" "}
