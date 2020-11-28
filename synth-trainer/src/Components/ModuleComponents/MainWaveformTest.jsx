5d891d669a6cc65b49ae38326529c34e3dc2a5e9
Jeremy Goldberg <ja_goldberg@yahoo.com>
2020-11-27 21:05:53 -0800

    modify user interaction

--- synth-trainer/src/Components/ModuleComponents/MainWaveformTest.jsx
+++ synth-trainer/src/Components/ModuleComponents/MainWaveformTest.jsx
@@ -8,93 +8,137 @@
 // ( ) after 10 the user can click see score to view results
 
 import React, { useState } from "react";
-import { Card, Button, Form } from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
+import Grid from "@material-ui/core/Grid";
 import PlayArrow from "@material-ui/icons/PlayArrow";
 import Repeat from "@material-ui/icons/Repeat";
+import { LinearProgress } from "@material-ui/core";
+
+let [score, current] = [0, 0];
+const waveforms = ["sine", "square", "sawtooth"];
 
 const MainWaveformTest = () => {
-  const waveforms = ["sine", "square", "sawtooth"];
   const [userGuess, setUserGuess] = useState([false, ""]);
   const [waveform, setWaveform] = useState("sine");
+  const [canPlayNewSound, setCanPlayNewSound] = useState(true);
 
   const handleButton = (event) => {
     const { name } = event.currentTarget;
 
     if (name === "playSound") {
       setUserGuess([false, ""]);
+      current += 1;
+      setCanPlayNewSound(false);
       setWaveform(waveforms[Math.floor(Math.random() * waveforms.length)]);
       // Play Sound
       // Set play to disabled
       // Set replay to enabled
+      return;
     }
 
-    if (name === "repeatSound") {
-      // Repeat sound
-    } else {
-      setUserGuess([true, name]);
+    if (name === "replaySound") {
+      // Replay sound
+      return;
     }
+    setUserGuess([true, name]);
+    if (name === waveform) {
+      score += 1;
+    }
+    setCanPlayNewSound(true);
   };
 
   return (
     <div>
       <Card bg="info">
         <Card.Title>Waveform Test</Card.Title>
+        <Card.Text>
+          {current === 0 ? "Press Play to Begin!" : "Wave: " + current}
+        </Card.Text>
+        <Card.Text>Score: {score}</Card.Text>
         <Card.Body>
-          <Button name="playSound" onClick={handleButton} disabled="false">
+          <LinearProgress
+            variant="determinate"
+            value={current * 10}
+          ></LinearProgress>
+          <Button
+            name="playSound"
+            onClick={handleButton}
+            disabled={!canPlayNewSound || (current === 10 && userGuess[0])}
+          >
             <PlayArrow />
           </Button>
-          <Button name="replaySond" onClick={handleButton}>
+          <Button
+            name="replaySound"
+            onClick={handleButton}
+            disabled={canPlayNewSound || (current === 10 && userGuess[0])}
+          >
             <Repeat />
           </Button>
-          <Form>
-            <Button
-              name="sawtooth"
-              variant={
-                userGuess[0] && waveform === "sawtooth"
-                  ? "success"
-                  : userGuess[0] &&
-                    userGuess[1] === "sawtooth" &&
-                    waveform !== "sawtooth"
-                  ? "danger"
-                  : "primary"
-              }
-              onClick={handleButton}
-            >
-              Sawtooth
-            </Button>
-            <Button
-              variant={
-                userGuess[0] && waveform === "sine"
-                  ? "success"
-                  : userGuess[0] &&
-                    userGuess[1] === "sine" &&
-                    waveform !== "sine"
-                  ? "danger"
-                  : "primary"
-              }
-              name="sine"
-              onClick={handleButton}
-            >
-              Sine
-            </Button>
-            <Button
-              variant={
-                userGuess[0] && waveform === "square"
-                  ? "success"
-                  : userGuess[0] &&
-                    userGuess[1] === "square" &&
-                    waveform !== "square"
-                  ? "danger"
-                  : "primary"
-              }
-              name="square"
-              onClick={handleButton}
-            >
-              Sine
-            </Button>
-          </Form>
+          <Grid container spacing={2}>
+            <Grid item>
+              <Button
+                name="sawtooth"
+                variant={
+                  userGuess[0] && waveform === "sawtooth"
+                    ? "success"
+                    : userGuess[0] &&
+                      userGuess[1] === "sawtooth" &&
+                      waveform !== "sawtooth"
+                    ? "danger"
+                    : "primary"
+                }
+                onClick={handleButton}
+                disabled={canPlayNewSound || (current === 10 && userGuess[0])}
+              >
+                Sawtooth
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button
+                variant={
+                  userGuess[0] && waveform === "sine"
+                    ? "success"
+                    : userGuess[0] &&
+                      userGuess[1] === "sine" &&
+                      waveform !== "sine"
+                    ? "danger"
+                    : "primary"
+                }
+                name="sine"
+                onClick={handleButton}
+                disabled={canPlayNewSound || (current === 10 && userGuess[0])}
+              >
+                Sine
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button
+                variant={
+                  userGuess[0] && waveform === "square"
+                    ? "success"
+                    : userGuess[0] &&
+                      userGuess[1] === "square" &&
+                      waveform !== "square"
+                    ? "danger"
+                    : "primary"
+                }
+                name="square"
+                onClick={handleButton}
+                disabled={canPlayNewSound || (current === 10 && userGuess[0])}
+              >
+                Sine
+              </Button>
+            </Grid>
+          </Grid>
         </Card.Body>
       </Card>
+      <Button
+        name="submit"
+        onClick={handleButton}
+        disabled={!(current === 10 && userGuess[0])}
+      >
+        Submit
+      </Button>
     </div>
   );
 };
